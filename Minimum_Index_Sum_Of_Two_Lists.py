19c698ceecf17b1451a2e8793bc5dcf3231a39d7
Shivansh Gupta <55259717+shivanshgupta93@users.noreply.github.com>
2021-07-05 23:01:17 -0400

    Python Algo Practice 0507

---
+++ Minimum_Index_Sum_Of_Two_Lists.py
@@ -0,0 +1,61 @@
+###Minimum Index Sum of Two Lists
+'''
+Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+ 
+
+Example 1:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+Output: ["Shogun"]
+Explanation: The only restaurant they both like is "Shogun".
+Example 2:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+Output: ["Shogun"]
+Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+Example 3:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Burger King","Tapioca Express","Shogun"]
+Output: ["KFC","Burger King","Tapioca Express","Shogun"]
+Example 4:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KNN","KFC","Burger King","Tapioca Express","Shogun"]
+Output: ["KFC","Burger King","Tapioca Express","Shogun"]
+Example 5:
+
+Input: list1 = ["KFC"], list2 = ["KFC"]
+Output: ["KFC"]
+ 
+
+Constraints:
+
+1 <= list1.length, list2.length <= 1000
+1 <= list1[i].length, list2[i].length <= 30
+list1[i] and list2[i] consist of spaces ' ' and English letters.
+All the stings of list1 are unique.
+All the stings of list2 are unique.
+'''
+
+##Solution:
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        index_sum = len(list1) + len(list2) + 1
+        index_value = 0
+        lst = []
+        
+        for key, value in enumerate(list1):
+            if value in list2:
+                index_value = list2.index(value)
+                ##print(key, value, index_value)
+                if index_sum > key + index_value:
+                    index_sum = key + index_value
+                    lst =[]
+                    lst.append(value)
+                elif index_sum == key + index_value:
+                    lst.append(value)
+                    
+        return lst
+        
